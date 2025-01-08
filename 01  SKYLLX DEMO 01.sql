2b7c8e7803aaeca89269866ef2224f3dc8f5211a
vishnu-P9074 <vishnuappuz354@gmail.com>
2025-01-08 16:36:28 +0530

    1 st commit

---
+++ 01  SKYLLX DEMO 01.sql
@@ -0,0 +1,3 @@
+CREATE TABLE e3 (e_id int,e_name varchar(20),e_dep varchar(20), e_mail varchar(30),e_qulication varchar (20),e_Mobile int,e_basic_salary int);
+INSERT INTO E3 VALUES (1,"Amal","slae","a@gmail.com","Dcp",225588,25600);
+SELECT * FROM E3
